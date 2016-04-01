16fc80c9fe802c1c74309518b93023606e67f91c
winona <winona0428@sina.com>
2016-04-01 16:39:27 +0800

    start up 1.1

--- src/enc/App.java
+++ src/enc/App.java
@@ -6,6 +6,7 @@ package enc;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -15,6 +16,7 @@ public class App extends Application {
   @Override
   public void start(Stage primaryStage) throws Exception {
     Parent root = FXMLLoader.load(getClass().getResource("../view/FileEncryptionSystem.fxml"));
+    primaryStage.setScene(new Scene(root, 600, 400));
     primaryStage.show();
   }
 }

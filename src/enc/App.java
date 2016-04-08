fd019e871ee84494b123aca6f36e0132ed1d831b
winona <winona0428@sina.com>
2016-04-08 14:54:19 +0800

    file chooser

--- src/enc/App.java
+++ src/enc/App.java
@@ -2,7 +2,7 @@
 
 package enc;
 
-import java.io.IOException;
+import enc.controller.FESController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -13,26 +13,33 @@ public class App extends Application {
   public static void main(String[] args) {
 
     // encrypt/ decrypt test
-    FileEncryptionSubSystem.encryption("src/test.txt", "password", "DES");
-    FileEncryptionSubSystem.decryption("encrypt.txt","password","DES");
+    //FileEncryptionSubSystem.encryption("src/test.txt", "password", "DES");
+    //FileEncryptionSubSystem.decryption("encrypt.txt","password","DES");
 
     // generate key test
-    KeyManagementSystem kgs = new KeyManagementSystem();
-    try {
-      kgs.createKeyStore("keyTest.txt","password","DES");
-    } catch (IOException e) {
-      System.out.println("File exists!");
-      e.printStackTrace();
-    }
-    kgs.generateNewKeys();
-    kgs.closeKeyStore();
+    //KeyManagementSystem kgs = new KeyManagementSystem();
+    //try {
+    //  kgs.createKeyStore("keyTest.txt","password","DES");
+    //} catch (IOException e) {
+    //  System.out.println("File exists!");
+    //  e.printStackTrace();
+    //}
+    //kgs.generateNewKeys();
+    //kgs.closeKeyStore();
 
-    //Application.launch(App.class, args);
+    Application.launch(App.class, args);
   }
+
   @Override
   public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("../view/FileEncryptionSystem.fxml"));
-    primaryStage.setScene(new Scene(root, 600, 400));
+    final FXMLLoader loader =
+      new FXMLLoader(getClass().getResource("../view/FileEncryptionSystem.fxml"));
+
+    final Parent root = loader.load();
+    final FESController controller = loader.getController();
+    controller.initStage(primaryStage);
+
+    primaryStage.setScene(new Scene(root, 600, 550));
     primaryStage.show();
   }
 }
--- src/enc/controller/FESController.java
+++ src/enc/controller/FESController.java
@@ -1,6 +1,33 @@
 package enc.controller;
 
-public class FESController{
 
+import java.io.File;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+
+public class FESController{
+  Stage stage;
+  final FileChooser fileChooser = new FileChooser();
+
+  public void onCreateNewFile(){
+    fileChooser.setTitle("Select Key Store File");
+    fileChooser.setInitialDirectory(
+      new File(System.getProperty("user.dir") + File.separator)
+    );
+    File file = fileChooser.showOpenDialog(stage);
+    if (file != null) {
+      openFile(file);
+    }
+  }
+
+  public void initStage(Stage stage){
+    this.stage = stage;
+  }
+
+
+  private void openFile(File file){
+
+  }
 
 }
